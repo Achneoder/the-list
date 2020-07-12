bb744a965dc85f782f3b791d73b92782d77ae1af
Maurice <me@codingoncatnip.de>
2020-07-12 19:46:46 +0200

    style: adds separation of list items

--- src/components/TList.vue
+++ src/components/TList.vue
@@ -1,29 +1,34 @@
 <template>
   <v-list two-line subheader>
     <v-subheader inset>Entries</v-subheader>
+    <div v-for="(item, index) in listItems" :key="item.id">
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-checkbox
+            :value="item.done"
+            @change="setDone(item.id)"
+          ></v-checkbox>
+        </v-list-item-avatar>
 
-    <v-list-item v-for="item in listItems" :key="item.id">
-      <v-list-item-avatar>
-        <v-checkbox :value="item.done" @change="setDone(item.id)"></v-checkbox>
-      </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item-content>
 
-      <v-list-item-content>
-        <v-list-item-title v-text="item.title"></v-list-item-title>
-      </v-list-item-content>
-
-      <v-list-item-action>
-        <v-btn icon @click="deleteItem(item.id)">
-          <v-icon color="red lighten-1">mdi-delete</v-icon>
-        </v-btn>
-      </v-list-item-action>
-    </v-list-item>
+        <v-list-item-action>
+          <v-btn icon @click="deleteItem(item.id)">
+            <v-icon color="red lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+      <v-divider v-if="index + 1 < listItems.length" :key="index"></v-divider>
+    </div>
   </v-list>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
-import ListStore from '../store/ListStore';
-import { getModule } from 'vuex-module-decorators';
-import { ListItem } from '../types/ListItem';
+import ListStore from "../store/ListStore";
+import { getModule } from "vuex-module-decorators";
+import { ListItem } from "../types/ListItem";
 
 @Component
 export default class ListCard extends Vue {
